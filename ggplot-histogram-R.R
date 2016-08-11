9cd79b068ae9c3ec0a587b9c2745e0a197c60f8b
Nishan D'Souza <nishand@gmail.com>
2016-08-10 18:49:24 -0700

    Added titles to ggplot histogram

--- ggplot-histogram-R.R
+++ ggplot-histogram-R.R
@@ -3,5 +3,5 @@ df <- data.frame(x)
 df$group = ifelse(df$x < 66, "50-65", ifelse(df$x < 75, "66-74", "75-89"))
 library(ggplot2)
 ggplot()
-ggplot(df, aes(x, fill = group)) + geom_histogram(binwidth=.95) + scale_fill_manual(values = c("50-65" = "#CD7F32", "66-74" = "#C0C0C0","75-89" = "gold"))  + xlim(50,88)
+ggplot(df, aes(x, fill = group)) + geom_histogram(binwidth=.95) + scale_fill_manual(values = c("50-65" = "#CD7F32", "66-74" = "#C0C0C0","75-89" = "gold"))  + xlim(50,88) + labs(x="Rating",y="Frequency")
 
