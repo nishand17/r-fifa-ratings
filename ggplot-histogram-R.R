c6b36edd9031946500337939f8073f7ce1502a60
Nishan D'Souza <nishand@gmail.com>
2016-08-15 14:01:58 -0700

    Added new Histogram

--- ggplot-histogram-R.R
+++ ggplot-histogram-R.R
@@ -2,6 +2,7 @@ x <- read.csv("ratings.csv", header =F)$V1
 df <- data.frame(x)
 df$group = ifelse(df$x < 66, "50-65", ifelse(df$x < 75, "66-74", "75-89"))
 library(ggplot2)
-ggplot()
-ggplot(df, aes(x, fill = group)) + geom_histogram(binwidth=2.9) + scale_fill_manual(values = c("50-65" = "#CD7F32", "66-74" = "#C0C0C0","75-89" = "gold"))  + xlim(50,88) + labs(x="Rating",y="Frequency")
+ggplot(df, aes(x, fill = group)) + geom_histogram(binwidth=2) + scale_fill_manual(values = c("50-65" = "#CD7F32", "66-74" = "#C0C0C0","75-89" = "gold"))  + xlim(50,88) + ggtitle("Full Histogram") + labs(x="Rating",y="Frequency") + theme(plot.title = element_text(family = "Trebuchet MS",  face="bold", size=15))
 
+
+              
