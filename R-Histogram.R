45ede2d6735592cab896ad93891bb3ba2dff2ce6
Nishan D'Souza <nishand@gmail.com>
2016-11-13 12:20:14 -0800

    Update R-Histogram.R

--- R-Histogram.R
+++ R-Histogram.R
@@ -1,3 +1,3 @@
 dat1 <- read.csv("mediumSet.csv", header=F)$V1
-hist(dat1, col="#C0C0C0", main="Distribution of Player Ratings", xlab ="Player Rating", ylab = "Frequency",xlim = c(40,99), breaks = 400)
+hist(dat1, col="#C0C0C0", main="Distribution of Player Ratings", xlab ="Player Rating (40-99)", ylab = "Frequency",xlim = c(40,99), breaks = 400)
 
